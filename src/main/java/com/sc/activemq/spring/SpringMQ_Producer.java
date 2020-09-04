f5fa5f285218cd91fb8fe4c4e865266ad9af9d6a
Jokerboozp <2929590482@qq.com>
2020-09-04 14:07:19 +0800

    0904update

---
+++ src/main/java/com/sc/activemq/spring/SpringMQ_Producer.java
@@ -0,0 +1,35 @@
+package com.sc.activemq.spring;
+
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Service;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+@Service
+public class SpringMQ_Producer {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    public static void main(String[] args) {
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SpringMQ_Producer produce = (SpringMQ_Producer) ctx.getBean("springMQ_Producer");
+
+        produce.jmsTemplate.send(new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                TextMessage textMessage = session.createTextMessage("********spring和ActiveMQ的整合例子******");
+                return textMessage;
+            }
+        });
+        System.out.println("send task over**********");
+    }
+}
